62e4a78caa343dece62183cae444f7bd7b242567
BojanMilutinovic1811 <bojanmilutinovic1811@gmail.com>
2019-01-03 18:59:44 +0100

    template engines

--- node_the_complete_guide/exercise_express/routes/shop.js
+++ node_the_complete_guide/exercise_express/routes/shop.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const path = require('path');
 
-const rootDir = require('../util/path')
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 
 const router = express.Router();
 
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+
+    const products = adminData.products;
+    res.render('shop', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+    })
 })
 
 
