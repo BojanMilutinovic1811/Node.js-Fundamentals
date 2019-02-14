4077a54c9b081dab54b17a23fc5596304da60991
BojanMilutinovic1811 <bojanmilutinovic1811@gmail.com>
2019-02-14 15:12:03 +0100

    update

--- restful_blog_app/app.js
+++ restful_blog_app/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const body_parser = require('body-parser')
+const method_override = require('method-override')
 
 
 mongoose.connect('mongodb+srv://bojan:bojan@mongodbtest-mjihi.mongodb.net/test?retryWrites=true', {useNewUrlParser:true}, ()=> console.log('db connected'))
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://bojan:bojan@mongodbtest-mjihi.mongodb.net/test?r
 app.use(express.static('public'))
 app.use(body_parser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
+app.use(method_override('_method'))
 
 
 
@@ -21,11 +23,9 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = new mongoose.model('blog', blogSchema);
 
-// Blog.create({
-//     title: 'Test Blog',
-//     image: 'Here should be an image',
-//     body: 'This is the body of our blog'
-// })
+app.get('/', (req ,res) => {
+    res.redirect('/blogs')
+})
 
 app.get('/blogs', (req, res) => {
 
@@ -39,10 +39,64 @@ app.get('/blogs', (req, res) => {
 
 })
 
-app.get('/', (req ,res) => {
-    res.redirect('/blogs')
+app.get('/blogs/new', (req, res) => {
+    res.render('new_blog')
 })
 
+app.post('/blogs', (req, res) => {
+    Blog.create(req.body.blog, (err, newBlog) => {
+        if(err) {
+            res.render('new_blog')
+        } else {
+            res.redirect('/blogs')
+        }
+    })
+})
+
+app.get('/blogs/:id', (req, res) => {
+   
+    Blog.findById(req.params.id, (err, result) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render('show_blog', {blog: result}) 
+        }
+    })
+})
+
+
+app.get('/blogs/:id/edit', (req, res) => {
+     
+    Blog.findById(req.params.id, (err, foundBlog) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.render('edit', {blog: foundBlog})
+        }
+    })   
+})
+ 
+app.put('/blogs/:id', (req, res) => {
+ 
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog)=> {
+        if(err) {
+            res.redirect('/blogs')
+        } else {
+            res.redirect('/blogs/'+req.params.id)
+        }
+    })
+})
+
+app.delete('/blogs/:id', (req, res) => {
+
+    Blog.findByIdAndDelete(req.params.id, (err, result) => {
+        if (err) {
+            res.redirect('/blogs')
+        } else {
+            res.redirect('/blogs')
+        }
+    })
+})
 
 const port = process.env.PORT || 3000; 
 
